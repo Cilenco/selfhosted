cf64d5054f2e4284973912d6d11892b051de267d
Christian Dielitz <christian@dielitz.net>
2023-11-17 17:37:03 +0100

    Improved setup script

--- setup.sh
+++ setup.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
-apt update
-apt upgrade
+if [[ $EUID > 0 ]]; then
+  echo "Please run as root"
+  exit
+fi
 
+echo "Setting up your RaspberryPi, this may take a while..."
+
+echo "Updating system"
+apt update > /dev/null
+apt upgrade > /dev/null
+
+echo "Downloading and installing docker"
 wget -O get-docker.sh https://get.docker.com
 
-sh get-docker.sh
-rm get-docker.sh
+sh get-docker.sh > /dev/null
+rm get-docker.sh > /dev/null
 
-sh nginx/install.sh
+echo "Disabling IPv6 Privacy Extension for a static IPv6 address"
+
+echo "net.ipv6.conf.all.use_tempaddr=0" >> /etc/sysctl.conf
+echo "net.ipv6.conf.default.use_tempaddr=0" >> /etc/sysctl.conf
