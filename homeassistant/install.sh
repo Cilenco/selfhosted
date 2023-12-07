5ec29c559a0d012d90c573514fff2f34d4da53df
Christian Dielitz <christian@dielitz.net>
2023-12-07 11:09:39 +0100

    Updated home assistant setup script

--- homeassistant/install.sh
+++ homeassistant/install.sh
@@ -5,5 +5,9 @@ mkdir config
 
 read -p "Enter domain: " DOMAIN && export DOMAIN
 
+PROXY_IP=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' nginx-proxy)
+
+export PROXY_IP
+
 (envsubst < templates/docker-compose.yml) > docker-compose.yml
 (envsubst < templates/configuration.yml) > config/configuration.yml
