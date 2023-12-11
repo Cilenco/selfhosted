fdc060c17f845668451ba6b78912ddf392a6149a
Christian Dielitz <christian@dielitz.net>
2023-12-11 20:35:55 +0100

    Updated files

--- homeassistant/install.sh
+++ homeassistant/install.sh
@@ -10,13 +10,18 @@ mkdir config
 
 read -p "Enter domain: " DOMAIN && export DOMAIN
 
-echo "net.ipv4.conf.all.forwarding=1" >> /etc/sysctl.conf
-echo "net.ipv6.conf.all.disable_ipv6=0" >> /etc/sysctl.conf
-echo "net.ipv6.conf.all.forwarding=1" >> /etc/sysctl.conf
-
 PROXY_IP=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' nginx-proxy)
 
 export PROXY_IP
 
 (envsubst < templates/docker-compose.yml) > docker-compose.yml
 (envsubst < templates/configuration.yml) > config/configuration.yml
+
+echo "net.ipv6.conf.all.disable_ipv6=0" >> /etc/sysctl.conf
+echo "net.ipv4.conf.all.forwarding=1" >> /etc/sysctl.conf
+echo "net.ipv6.conf.all.forwarding=1" >> /etc/sysctl.conf
+
+modprobe ip6table_filter # persistant
+echo "ip6table_filter" >> /etc/modules
+
+sudo sysctl -p /etc/sysctl.conf 
