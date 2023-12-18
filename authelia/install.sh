73e48cb5dce3c14f45f22e1a95cd9038f65d202c
Christian Dielitz <christian@dielitz.net>
2023-12-19 00:04:39 +0100

    Added authelia as login auth for Outline

--- authelia/install.sh
+++ authelia/install.sh
@@ -4,7 +4,11 @@ read -p "Enter domain: " DOMAIN && export DOMAIN
 
 export ENCRYPTION_KEY=$(openssl rand -hex 64)
 export SESSION_SECRET=$(openssl rand -hex 64)
+
+export HMAC_SECRET=$(openssl rand -hex 64)
 export JWT_SECRET=$(openssl rand -hex 64)
+
+export PRIVATE_KEY=$(openssl genrsa 4096)
 
 rm -rf config
 mkdir config
