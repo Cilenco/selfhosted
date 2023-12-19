36a6249eb58a0dd1d364194e978bfe44b4409704
Christian Dielitz <christian@dielitz.net>
2023-12-19 12:20:19 +0100

    Updated OTBR container

--- otbr/install.sh
+++ otbr/install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-docker network create -d macvlan --ipv6 --subnet=192.168.178.0/24 --gateway=192.168.178.1 --subnet=fd00:4a82:739b::/64 --gateway=fd00:4a82:739b::ca0e:14ff:fe8d:8d84 -o parent=eth0 home_network
-
 read -p "Enter domain: " DOMAIN && export DOMAIN
 
 (envsubst < templates/docker-compose.yml) > docker-compose.yml
