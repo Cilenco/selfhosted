dd271b2ba50a75f4c0c5b6657db77ac8babff7e2
Christian Dielitz <christian@dielitz.net>
2023-12-31 15:39:34 +0100

    Removed zsh

--- setup.sh
+++ setup.sh
@@ -7,12 +7,6 @@ fi
 
 echo "Setting up your RaspberryPi, this may take a while..."
 
-echo "Setting up ZSH as shell"
-URL=https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
-
-apt install zsh
-sh -c "$(curl -fsSL $URL)"
-
 echo "Downloading and installing docker"
 wget -O get-docker.sh https://get.docker.com
 
