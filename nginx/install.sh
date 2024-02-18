2f9219c3fec3962fad4784f1a5887e7ec29e4116
Christian Dielitz <christian@dielitz.net>
2024-02-18 17:49:00 +0100

    Added upload file size config for Nextcloud

--- nginx/install.sh
+++ nginx/install.sh
@@ -2,6 +2,9 @@
 
 docker volume create certs > /dev/null
 
+rm -rf config
+mkdir config
+
 read -p "Enter Domain: " DOMAIN && export DOMAIN
 read -p "Enter E-Mail address: " EMAIL && export EMAIL
 
@@ -11,3 +14,6 @@ read -p "Enter Ionos API secret: " SECRET
 echo "$PREFIX.$SECRET" > credentials
 
 (envsubst < templates/docker-compose.yml) > docker-compose.yml
+
+echo "client_max_body_size 10G;" > config/uploadsize.conf
+echo "proxy_request_buffering off;" > config/uploadsize.conf
