89acc9d6e532c3c98ee36bed2d6c2e2c1747334d
Christian Dielitz <christian@dielitz.net>
2024-06-21 18:01:32 +0200

    Improved create user script

--- authelia/create_user.sh
+++ authelia/create_user.sh
@@ -4,8 +4,10 @@ read -p "Username: " USER_NAME && export USER_NAME
 read -p "Display name: " DISPLAY_NAME && export DISPLAY_NAME
 
 read -p "E-Mail: " E_MAIL && export E_MAIL
-read -s -p "Password: " PASSWORD && export PASSWORD
+read -s -p "Password: " PW
 
 echo # Do not print % after silent read
+SALT=$(openssl rand -hex 16)
 
+export PASSWORD=$(echo $PW | argon2 $SALT -id -t 1 -l 32 -m 10 -p 8 -e)
 (envsubst < templates/user_template.yml) >> config/users_database.yml
