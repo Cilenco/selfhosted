0f08d79362a1970e0b742482f1f708b22bf0e1ee
Christian Dielitz <christian@dielitz.net>
2024-06-25 07:15:45 +0200

    Updated spliit config

--- spliit/install.sh
+++ spliit/install.sh
@@ -4,5 +4,3 @@ CONFIG_FILE=$HOME/config.ini
 
 export DOMAIN=$(crudini --get $CONFIG_FILE general domain)
 (envsubst < templates/docker-compose.yml) > docker-compose.yml
-
-(envsubst < templates/env.spliit) > env.spliit
