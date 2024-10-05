f065f67e9181935a3f8df2dd13b612a7b539c434
Christian Dielitz <christian@dielitz.net>
2024-10-05 17:52:09 +0200

    Removed unused services

--- homeassistant/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-CONFIG_FILE=$HOME/config.ini
-
-export DOMAIN=$(crudini --get $CONFIG_FILE general domain)
-(envsubst < templates/docker-compose.yml) > docker-compose.yml
-
-echo "Finished setup. Please execute 'sudo modprobe ip6table_filter' now"
--- spliit/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-CONFIG_FILE=$HOME/config.ini
-
-export DOMAIN=$(crudini --get $CONFIG_FILE general domain)
-(envsubst < templates/docker-compose.yml) > docker-compose.yml
