e319904fac33442f863ee7cf11d32699ebf230e8
Christian Dielitz <christian@dielitz.net>
2024-10-21 12:46:51 +0200

    Added test nginx container

---
+++ test/docker/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Start nginx server
+nginx -g daemon off;
+
+/acme/acme.sh --issue                         \
+              --dns dns_ionos                 \
+              --server letsencrypt            \
+              --domain *.$DOMAIN              \
+              --domain $DOMAIN
+
+/acme/acme.sh --install-cert                  \
+              --domain    $DOMAIN             \
+              --key-file  /certs/$DOMAIN.crt  \
+              --cert-file /certs/$DOMAIN.key  \
+              --reloadcmd "nginx -s reload"
