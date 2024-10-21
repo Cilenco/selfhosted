d6a882b504363a9b146a82d45e1fd2f9845b6879
Christian Dielitz <christian@dielitz.net>
2024-10-21 11:22:25 +0200

    Added test nginx container

--- test/install.sh
+++ test/install.sh
@@ -13,7 +13,7 @@ export EMAIL=$(crudini --get $CONFIG_FILE general email)
 export PREFIX=$(crudini --get $CONFIG_FILE ionos prefix)
 export SECRET=$(crudini --get $CONFIG_FILE ionos secret)
 
-(envsubst < templates/docker-compose.yml) > docker-compose.yml
+(envsubst < docker/docker-compose.yml) > docker-compose.yml
 
 echo "client_max_body_size 10G;" > config/uploadsize.conf
 echo "proxy_request_buffering off;" > config/uploadsize.conf
