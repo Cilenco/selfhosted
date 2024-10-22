daccd544343fe591972ec5187f97fb8648ec4542
Christian Dielitz <christian@fedora.fritz.box>
2024-10-22 12:05:44 +0200

    Added adguard podman config

--- adguard/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-CONFIG_FILE=$HOME/config.ini
-
-rm -rf workdir
-rm -rf confdir
-
-export DOMAIN=$(crudini --get $CONFIG_FILE general domain)
-(envsubst < templates/docker-compose.yml) > docker-compose.yml
