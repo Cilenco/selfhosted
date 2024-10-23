264be3b9b7735021872964f08fcc2e518e5574bc
Christian Dielitz <christian@fedora.fritz.box>
2024-10-23 10:51:39 +0200

    Adjusted setup script

--- install.sh
+++ install.sh
@@ -1,68 +1,33 @@
 #!/bin/bash
 
-#if [[ $EUID > 0 ]]; then
-#  echo "Please run as root"
-#  exit
-#fi
+if [[ $EUID > 0 ]]; then
+  echo "Please run as root"
+  exit
+fi
 
-CONFIG_FILE=$HOME/config.ini
-
-echo "Installing system updates as well as make and crudini"
+echo "Installing system updates as well as podman and make"
 sudo DEBIAN_FRONTEND=noninteractive apt-get -yqq update
 sudo DEBIAN_FRONTEND=noninteractive apt-get -yqq upgrade
 
-sudo DEBIAN_FRONTEND=noninteractive apt-get -yqq install make
-sudo DEBIAN_FRONTEND=noninteractive apt-get -yqq install crudini
+sudo DEBIAN_FRONTEND=noninteractive apt-get -yqq install make podman
 
 read -p "Enter domain: " DOMAIN
-read -p "Enter E-Mail: " EMAIL
+echo $DOMAIN >> /etc/selfhosted.env
 
-crudini --set $CONFIG_FILE general domain $DOMAIN
-crudini --set $CONFIG_FILE general email $EMAIL
+read -p "Enter Ionos API prefix: " IONOS_PREFIX
+read -p "Enter Ionos API secret: " IONOS_SECRET
 
-read -p "Enter Ionos API prefix: " PREFIX
-read -p "Enter Ionos API secret: " SECRET
-
-crudini --set $CONFIG_FILE ionos prefix $PREFIX
-crudini --set $CONFIG_FILE ionos secret $SECRET
+podman secret create --env=true ionos_prefix IONOS_PREFIX
+podman secret create --env=true ionos_secret IONOS_SECRET
 
 read -p "Enter SMTP server: " SMTP_SERVER
 read -p "Enter SMTP port: " SMTP_PORT
 
+echo $SMTP_SERVER >> /etc/selfhosted.env
+echo $SMTP_PORT >> /etc/selfhosted.env
+
 read    -p "Enter SMTP username: " SMTP_USERNAME
 read -s -p "Enter SMTP password: " SMTP_PASSWORD
 
-crudini --set $CONFIG_FILE smtp server $SMTP_SERVER
-crudini --set $CONFIG_FILE smtp port $SMTP_PORT
-
-crudini --set $CONFIG_FILE smtp username $SMTP_USERNAME
-crudini --set $CONFIG_FILE smtp password $SMTP_PASSWORD
-
-echo "Downloading and installing docker"
-wget -O get-docker.sh https://get.docker.com
-
-sh get-docker.sh > /dev/null
-rm get-docker.sh > /dev/null
-
-[ $(getent group docker) ] || sudo groupadd docker
-sudo usermod -aG docker ${SUDO_USER:-$USER}
-
-#if [[ 0 > 0 ]]; then
-#  groupadd docker
-#  usermod -aG docker admin
-
-#  echo "Disabling IPv6 Privacy Extension for a static IPv6 address"
-#
-#  echo "net.ipv6.conf.all.use_tempaddr=0" >> /etc/sysctl.conf
-#  echo "net.ipv6.conf.default.use_tempaddr=0" >> /etc/sysctl.conf
-#
-#  echo -e "Setup process of your Raspberry finished successfully."
-#  echo -e "Please restart the device now to apply all changes.\n"
-#
-#  read -r -p "Would you like to restart the device now? [Y/n] " reboot;
-#
-#  if [ "$reboot" != "" ]; then echo; fi
-#  if [ "$reboot" = "${reboot#[Nn]}" ]; then
-#    shutdown -r now
-#  fi
-#fi
+podman secret create --env=true smtp_username SMTP_USERNAME
+podman secret create --env=true smtp_password SMTP_PASSWORD
