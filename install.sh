7b2272d2d97af24ef31bc50afa5f33f9c18f05ef
Christian Dielitz <christian@fedora.fritz.box>
2024-10-23 15:15:40 +0200

    Fixed setup script

--- install.sh
+++ install.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
 
-if [[ $EUID > 0 ]]; then
-  echo "Please run as root"
-  exit
-fi
-
-echo "Installing system updates as well as podman"
-sudo DEBIAN_FRONTEND=noninteractive apt-get -yqq update
-sudo DEBIAN_FRONTEND=noninteractive apt-get -yqq upgrade
-
-sudo DEBIAN_FRONTEND=noninteractive apt-get -yqq install podman
-
 read -p "Enter domain: " DOMAIN
-echo "DOMAIN=$DOMAIN" >> /etc/selfhosted.env
+echo "DOMAIN=$DOMAIN" >> ~/selfhosted.env
 
 read -p "Enter Ionos API prefix: " IONOS_PREFIX
 read -p "Enter Ionos API secret: " IONOS_SECRET
@@ -23,8 +12,8 @@ printf $IONOS_SECRET | podman secret create ionos_secret -
 read -p "Enter SMTP server: " SMTP_SERVER
 read -p "Enter SMTP port: " SMTP_PORT
 
-echo "SMTP_SERVER=$SMTP_SERVER" >> /etc/selfhosted.env
-echo "SMTP_PORT=$SMTP_PORT" >> /etc/selfhosted.env
+echo "SMTP_SERVER=$SMTP_SERVER" >> ~/selfhosted.env
+echo "SMTP_PORT=$SMTP_PORT" >> ~/selfhosted.env
 
 read    -p "Enter SMTP username: " SMTP_USERNAME
 read -s -p "Enter SMTP password: " SMTP_PASSWORD
