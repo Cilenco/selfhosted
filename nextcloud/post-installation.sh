170eed95c7dfc2722e3b7fea4f0aa9f72aff7744
Christian Dielitz <christian@fedora.fritz.box>
2025-01-05 21:14:01 +0100

    Improved nextcloud and authelia config

--- nextcloud/post-installation.sh
+++ nextcloud/post-installation.sh
@@ -1,21 +1,42 @@
 #!/bin/bash
 
-php occ app:install  calendar
-php occ app:enable   calendar
+#################################################
+# Setup basic config                            #
+#################################################
 
-php occ app:install  contacts
-php occ app:enable   contacts
+php occ config:system:set trusted_domains 2 --type="string" --value="cloud.$DOMAIN"
 
-php occ app:install  user_oidc
-php occ app:enable   user_oidc
+php occ config:system:set default_language --type="string" --value="de"
+php occ config:system:set default_phone_region --type="string" --value="DE"
+php occ config:system:set default_locale --type="string" --value="de_DE"
 
-php occ app:install  unroundedcorners
-php occ app:enable   unroundedcorners
+php occ config:system:set default_timezone --type="string" --value="Europe/Berlin"
 
-php occ app:disable  dashboard
-php occ app:disable  weather_status
-php occ app:disable  user_status
+php occ config:system:set hide_login_form --type="boolean" --value="false"
+php occ config:system:set updatechecker --type="boolean" --value="false"
+
+php occ config:system:set defaultapp --type="string" --value="files"
+
+#"files.chunked_upload.max_size" => 4 * 1024 * 1024 * 1024, // 4 GB
+
+#################################################
+# Configure additional apps                     #
+#################################################
+
+php occ app:install calendar
+php occ app:install contacts
+php occ app:install user_oidc
+
+php occ app:install unroundedcorners
+
+php occ app:disable dashboard
+php occ app:disable weather_status
+php occ app:disable user_status
+
+#################################################
+# Setup Authelia authentication                 #
+#################################################
 
 php occ user_oidc:provider authelia \
     --clientid="nextcloud" --clientsecret=$CLIENT_SECRET \
-    --discoveryuri="http://authelia/openid-configuration"
+    --discoveryuri="https://auth.$DOMAIN/.well-known/openid-configuration
