d6f53923f668b26ecd6363542b54e0755c37e185
Christian Dielitz <christian@fedora.fritz.box>
2025-01-18 13:59:42 +0100

    Overall improvements

--- setup.sh
+++ setup.sh
@@ -7,48 +7,49 @@
 read -p "Enter domain: " DOMAIN
 read -p "Enter E-Mail: " E_MAIL
 
-printf $DOMAIN | podman secret create DOMAIN -
-printf $E_MAIL | podman secret create E_MAIL -
+printf $DOMAIN | podman secret create DOMAIN - >/dev/null
+printf $E_MAIL | podman secret create E_MAIL - >/dev/null
 
 read -p "Enter Ionos API prefix: " IONOS_PREFIX
 read -p "Enter Ionos API secret: " IONOS_SECRET
 
-printf $IONOS_PREFIX | podman secret create IONOS_PREFIX -
-printf $IONOS_SECRET | podman secret create IONOS_SECRET -
+printf $IONOS_PREFIX | podman secret create IONOS_PREFIX - >/dev/null
+printf $IONOS_SECRET | podman secret create IONOS_SECRET - >/dev/null
 
 read -p "Enter SMTP host: " SMTP_HOST
 read -p "Enter SMTP port: " SMTP_PORT
 
-printf $SMTP_HOST | podman secret create SMTP_HOST -
-printf $SMTP_PORT | podman secret create SMTP_PORT -
+printf $SMTP_HOST | podman secret create SMTP_HOST - >/dev/null
+printf $SMTP_PORT | podman secret create SMTP_PORT - >/dev/null
 
 read    -p "Enter SMTP username: " SMTP_USERNAME
 read -s -p "Enter SMTP password: " SMTP_PASSWORD
 
-printf $SMTP_USERNAME | podman secret create SMTP_USERNAME -
-printf $SMTP_PASSWORD | podman secret create SMTP_PASSWORD -
+printf $SMTP_USERNAME | podman secret create SMTP_USERNAME - >/dev/null
+printf $SMTP_PASSWORD | podman secret create SMTP_PASSWORD - >/dev/null
 
 ########################
 ########################
 
-printf $(openssl rand -hex 64) | podman secret create NEXTCLOUD_CLIENT_SECRET -
+printf $(openssl rand -hex    64) | podman secret create NEXTCLOUD_CLIENT_SECRET - >/dev/null
+printf $(openssl rand -hex    12) | podman secret create NEXTCLOUD_DB_USERNAME   - >/dev/null
+printf $(openssl rand -base64 24) | podman secret create NEXTCLOUD_DB_PASSWORD   - >/dev/null
 
-printf $(openssl rand -hex 64) | podman secret create POSTGRES_USERNAME -
-printf $(openssl rand -hex 64) | podman secret create POSTGRES_PASSWORD -
+printf $(openssl rand -hex    32) | podman secret create OUTLINE_SECRET_KEY    - >/dev/null
+printf $(openssl rand -hex    64) | podman secret create OUTLINE_CLIENT_SECRET - >/dev/null
+printf $(openssl rand -hex    32) | podman secret create OUTLINE_UTILS_SECRET  - >/dev/null
+printf $(openssl rand -hex    12) | podman secret create OUTLINE_DB_USERNAME   - >/dev/null
+printf $(openssl rand -base64 24) | podman secret create OUTLINE_DB_PASSWORD   - >/dev/null
 
-printf $(openssl rand -hex 32) | podman secret create OUTLINE_SECRET_KEY -
-printf $(openssl rand -hex 64) | podman secret create OUTLINE_CLIENT_SECRET -
-printf $(openssl rand -hex 32) | podman secret create OUTLINE_UTILS_SECRET -
+printf $(openssl rand -hex    64) | podman secret create AUTHELIA_JWT_SECRET             - >/dev/null
+printf $(openssl rand -hex    64) | podman secret create AUTHELIA_OIDC_HMAC_SECRET       - >/dev/null
+printf $(openssl rand -hex    64) | podman secret create AUTHELIA_STORAGE_ENCRYPTION_KEY - >/dev/null
+printf $(openssl rand -hex    64) | podman secret create AUTHELIA_SESSION_SECRET         - >/dev/null
 
-printf $(openssl rand -hex 64) | podman secret create AUTHELIA_JWT_SECRET -
-printf $(openssl rand -hex 64) | podman secret create AUTHELIA_OIDC_HMAC_SECRET -
-printf $(openssl rand -hex 64) | podman secret create AUTHELIA_STORAGE_ENCRYPTION_KEY -
-printf $(openssl rand -hex 64) | podman secret create AUTHELIA_SESSION_SECRET -
-
-printf $(openssl rand -hex 64) | podman secret create LDAP_JWT_KEY -
-printf $(openssl rand -hex 64) | podman secret create LDAP_KEY_SEED -
-printf $(openssl rand -hex 12) | podman secret create LDAP_PASSWORD -
-printf $(openssl rand -hex  8) | podman secret create LDAP_USERNAME -
+printf $(openssl rand -hex    64) | podman secret create LDAP_JWT_KEY  - >/dev/null
+printf $(openssl rand -hex    64) | podman secret create LDAP_KEY_SEED - >/dev/null
+printf $(openssl rand -hex    12) | podman secret create LDAP_USERNAME - >/dev/null
+printf $(openssl rand -base64 24) | podman secret create LDAP_PASSWORD - >/dev/null
 
 echo -e "\n\n"
 
