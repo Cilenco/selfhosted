649053178bf49f71cf3bac147c0cd7b86a07f622
Christian Dielitz <christian@fedora.fritz.box>
2025-02-15 09:27:39 +0100

    Fixed services

--- nextcloud/post-installation.sh
+++ nextcloud/post-installation.sh
@@ -3,6 +3,7 @@
 #################################################
 # Setup basic config                            #
 #################################################
+php occ config:system:set allow_local_remote_servers --type="boolean" --value="true"
 
 php occ config:system:set trusted_domains 2 --type="string" --value="cloud.$DOMAIN"
 
@@ -39,57 +40,57 @@ php occ app:disable user_status
 #################################################
 BASE_DN="dc=example,dc=com"
 
-occ ldap:create-empty-config
+php occ ldap:create-empty-config
 
-occ ldap:set-config s01 ldapHost "ldap://ldap"
-occ ldap:set-config s01 ldapPort 3890
+php occ ldap:set-config s01 ldapHost "ldap://ldap"
+php occ ldap:set-config s01 ldapPort 3890
 
 # EDIT: admin user
-occ ldap:set-config s01 ldapAgentName "uid=$LDAP_USERNAME,ou=people,$BASE_DN"
-occ ldap:set-config s01 ldapAgentPassword "$LDAP_PASSWORD"
+php occ ldap:set-config s01 ldapAgentName "uid=$LDAP_USERNAME,ou=people,$BASE_DN"
+php occ ldap:set-config s01 ldapAgentPassword "$LDAP_PASSWORD"
 
 # Set LDAP admin group as admins in Nextcloud
 php occ ldap:promote-group  "admin"
 
 # EDIT: Base DN
-occ ldap:set-config s01 ldapBase "$BASE_DN"
-occ ldap:set-config s01 ldapBaseUsers "$BASE_DN"
-occ ldap:set-config s01 ldapBaseGroups "$BASE_DN"
+php occ ldap:set-config s01 ldapBase "$BASE_DN"
+php occ ldap:set-config s01 ldapBaseUsers "$BASE_DN"
+php occ ldap:set-config s01 ldapBaseGroups "$BASE_DN"
 
-occ ldap:set-config s01 ldapConfigurationActive 1
-occ ldap:set-config s01 ldapLoginFilter "(&(objectclass=person)(uid=%uid))"
+php occ ldap:set-config s01 ldapConfigurationActive 1
+php occ ldap:set-config s01 ldapLoginFilter "(&(objectclass=person)(uid=%uid))"
 
 # EDIT: nextcloud_users group, contains the users who can login to Nextcloud
-occ ldap:set-config s01 ldapUserFilter "(&(objectclass=person)(memberOf=cn=nextcloud,ou=groups,$BASE_DN))"
-occ ldap:set-config s01 ldapUserFilterMode 0
-occ ldap:set-config s01 ldapUserFilterObjectclass person
-occ ldap:set-config s01 turnOnPasswordChange 0
-occ ldap:set-config s01 ldapCacheTTL 600
-occ ldap:set-config s01 ldapExperiencedAdmin 0
-occ ldap:set-config s01 ldapGidNumber gidNumber
+php occ ldap:set-config s01 ldapUserFilter "(&(objectclass=person)(memberOf=cn=nextcloud,ou=groups,$BASE_DN))"
+php occ ldap:set-config s01 ldapUserFilterMode 0
+php occ ldap:set-config s01 ldapUserFilterObjectclass person
+php occ ldap:set-config s01 turnOnPasswordChange 0
+php occ ldap:set-config s01 ldapCacheTTL 600
+php occ ldap:set-config s01 ldapExperiencedAdmin 0
+php occ ldap:set-config s01 ldapGidNumber gidNumber
 
 # EDIT: list of application groups
-occ ldap:set-config s01 ldapGroupFilter "(&(objectclass=groupOfUniqueNames)(|(cn=friends)(cn=family)))"
+php occ ldap:set-config s01 ldapGroupFilter "(&(objectclass=groupOfUniqueNames)(|(cn=friends)(cn=family)))"
 
 # EDIT: list of application groups
-occ ldap:set-config s01 ldapGroupFilterGroups "friends;family"
-occ ldap:set-config s01 ldapGroupFilterMode 0
-occ ldap:set-config s01 ldapGroupDisplayName cn
-occ ldap:set-config s01 ldapGroupFilterObjectclass groupOfUniqueNames
-occ ldap:set-config s01 ldapGroupMemberAssocAttr uniqueMember
-occ ldap:set-config s01 ldapEmailAttribute "mail"
-occ ldap:set-config s01 ldapLoginFilterEmail 0
-occ ldap:set-config s01 ldapLoginFilterUsername 1
-occ ldap:set-config s01 ldapMatchingRuleInChainState unknown
-occ ldap:set-config s01 ldapNestedGroups 0
-occ ldap:set-config s01 ldapPagingSize 500
-occ ldap:set-config s01 ldapTLS 0
-occ ldap:set-config s01 ldapUserAvatarRule default
-occ ldap:set-config s01 ldapUserDisplayName displayname
-occ ldap:set-config s01 ldapUserFilterMode 1
-occ ldap:set-config s01 ldapUuidGroupAttribute auto
-occ ldap:set-config s01 ldapUuidUserAttribute auto
-occ ldap:set-config s01 ldapExpertUsernameAttr user_id
+php occ ldap:set-config s01 ldapGroupFilterGroups "friends;family"
+php occ ldap:set-config s01 ldapGroupFilterMode 0
+php occ ldap:set-config s01 ldapGroupDisplayName cn
+php occ ldap:set-config s01 ldapGroupFilterObjectclass groupOfUniqueNames
+php occ ldap:set-config s01 ldapGroupMemberAssocAttr uniqueMember
+php occ ldap:set-config s01 ldapEmailAttribute "mail"
+php occ ldap:set-config s01 ldapLoginFilterEmail 0
+php occ ldap:set-config s01 ldapLoginFilterUsername 1
+php occ ldap:set-config s01 ldapMatchingRuleInChainState unknown
+php occ ldap:set-config s01 ldapNestedGroups 0
+php occ ldap:set-config s01 ldapPagingSize 500
+php occ ldap:set-config s01 ldapTLS 0
+php occ ldap:set-config s01 ldapUserAvatarRule default
+php occ ldap:set-config s01 ldapUserDisplayName displayname
+php occ ldap:set-config s01 ldapUserFilterMode 1
+php occ ldap:set-config s01 ldapUuidGroupAttribute auto
+php occ ldap:set-config s01 ldapExpertUsernameAttr user_id
+php occ ldap:set-config s01 ldapUuidUserAttribute auto
 
 #################################################
 # Setup Authelia authentication                 #
@@ -97,4 +98,4 @@ occ ldap:set-config s01 ldapExpertUsernameAttr user_id
 
 php occ user_oidc:provider authelia \
     --clientid="nextcloud" --clientsecret=$CLIENT_SECRET \
-    --discoveryuri="https://auth.$DOMAIN/.well-known/openid-configuration
+    --discoveryuri="https://auth.$DOMAIN/.well-known/openid-configuration"
