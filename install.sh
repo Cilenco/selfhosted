8404e81c97945644977cee20f34da3a8df66ac26
Christian Dielitz <christian@dielitz.net>
2025-08-30 15:52:25 +0200

    Removed ansible and added bootstrap scripts

---
+++ install.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+if [[ $EUID > 0 ]]; then
+  echo "Please run as root"
+  exit
+fi
+
+UNPRIVILEGED_PORT_FILE=/etc/sysctl.d/10-unprivileged-port.conf
+
+apt install podman    # Install container runtime
+apt install bzip2     # Used by autorestic install
+
+# Install restic and autorestic for backing up all data
+wget -qO - https://raw.githubusercontent.com/cupcakearmy/autorestic/master/install.sh | bash
+
+# Gather general information for
+echo -e "Setting up your machine for selfhosting\n\n"
+
+read -p "Enter domain: " DOMAIN
+read -p "Enter E-Mail: " E_MAIL
+
+read -p "Enter Ionos API prefix: " IONOS_PREFIX
+read -p "Enter Ionos API secret: " IONOS_SECRET
+
+read -p "Enter SMTP host: " SMTP_HOST
+read -p "Enter SMTP port: " SMTP_PORT
+
+read    -p "Enter SMTP username: " SMTP_USERNAME
+read -s -p "Enter SMTP password: " SMTP_PASSWORD
+
+# Create podman secrets
+printf $DOMAIN | podman secret create DOMAIN -
+printf $E_MAIL | podman secret create E_MAIL -
+
+printf $IONOS_PREFIX | podman secret create IONOS_PREFIX -
+printf $IONOS_SECRET | podman secret create IONOS_SECRET -
+
+printf $SMTP_HOST | podman secret create SMTP_HOST -
+printf $SMTP_PORT | podman secret create SMTP_PORT -
+
+printf $SMTP_USERNAME | podman secret create SMTP_USERNAME -
+printf $SMTP_PASSWORD | podman secret create SMTP_PASSWORD -
+
+# Allow unprivileged port binding
+if [[ -e $UNPRIVILEGED_PORT_FILE ]]; then
+  echo "net.ipv4.ip_unprivileged_port_start=53" >> $UNPRIVILEGED_PORT_FILE
+fi
