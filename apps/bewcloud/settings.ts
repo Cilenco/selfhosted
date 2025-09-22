a7bb128014f7ca4730d5e9cc9d7ae0ea5d0842ee
Christian Dielitz <christian@dielitz.net>
2025-09-22 19:01:34 +0200

    Added BewCloud service

---
+++ apps/bewcloud/settings.ts
@@ -0,0 +1,38 @@
+import { Config, PartialDeep } from './lib/types.ts';
+
+/** Check the Config type for all the possible options and instructions. */
+const config: PartialDeep<Config> = {
+  auth: {
+    baseUrl: 'https://cloud.${DOMAIN}',
+    allowSignups: false,
+    enableEmailVerification: false,
+    enableForeverSignup: false,
+    enableMultiFactor: false,
+    enableSingleSignOn: true,
+    singleSignOnUrl: 'https://auth.${DOMAIN}',
+    singleSignOnScopes: ['openid', 'email'],
+    singleSignOnEmailAttribute: 'email',
+  },
+  files: {
+    rootPath: 'data-files',
+    allowPublicSharing: true,
+  },
+  core: {
+    enabledApps: ['news', 'notes', 'photos'], //, 'contacts', 'calendar'],
+  },
+  // visuals: {
+  //   title: 'My own cloud',
+  //   description: 'This is my own cloud!',
+  //   helpEmail: '',
+  // },
+  // contacts: {
+  //   enableCardDavServer: true,
+  //   cardDavUrl: 'http://radicale:5232',
+  // },
+  // calendar: {
+  //   enableCalDavServer: true,
+  //   calDavUrl: 'http://radicale:5232',
+  // },
+};
+
+export default config;
